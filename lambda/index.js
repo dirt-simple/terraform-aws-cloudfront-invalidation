6eda5589e4931a7fd541a80bfdd37e048fc50c80
Joe Duran <joeduran8@gmail.com>
2019-09-19 14:09:22 -0500

    async

--- lambda/index.js
+++ lambda/index.js
@@ -1,92 +1,74 @@
 "use strict";
-const AWS = require("aws-sdk");
-const cloudfront = new AWS.CloudFront();
-const sqs = new AWS.SQS({region: process.env.AWS_REGION});
+
+import { logWarning, logRetry, log } from "./loggers";
+import {
+  sendSqsMessage,
+  createCloudfrontInvalidation,
+  makeInvalidationParams,
+  makeSnsParams
+} from "./aws";
+import { asyncForEach, parseMessage } from "./utils";
 
 const NUM_OF_RETRIES = process.env.INVALIDATION_MAX_RETRIES;
-const RETRY_TIMOUT = process.env.INVALIDATION_RETRY_TIMOUT;
 
-exports.handler = (event, context, callback) => {
+const handleInvalidationError = async err => {
+  if (err.code !== "TooManyInvalidationsInProgress") {
+    const warning = `ignoring error: ${err}.`;
+    logWarning(warning);
+    return Promise.resolve();
+  }
 
-    event.Records.forEach(record => {
+  if (shouldRetry(message)) {
+    try {
+      return await retryMessage(message);
+    } catch (err) {
+      log(err);
+      return Promise.resolve("failed to send message for retry");
+    }
+  } else {
+    const warning = `Failed after ${NUM_OF_RETRIES} retries`;
+    logWarning(warning);
+    return Promise.resolve(warning);
+  }
+};
 
-        const body = JSON.parse(record.body);
-        const message = JSON.parse(body.Message);
+async function retryMessage(message) {
+  message.retry_count++;
+  // This Message must be in SNS => SQS Format for incoming parsing to work
+  const params = makeSnsParams(message, record.eventSourceARN);
+  logRetry(
+    `retrying ${invalidationParams.DistributionId}:${invalidationParams.Paths}`,
+    params
+  );
+  return await sendSqsMessage(params);
+}
 
-        console.info("SQS Message: ", message);
+function shouldRetry(message) {
+  let retried = message.retry_count || 0;
+  return retried < NUM_OF_RETRIES - 1;
+}
 
-        if (!message.distribution_id || !message.path) {
-            const msg = `[WARNING] bad format. desired SNS message format: {\"distribution_id\": \"<distid>\", \"path\": \"/a/path/*\"}`;
-            console.log(msg);
-            callback(null, msg);
-            return;
-        }
+exports.handler = async function(event) {
+  await asyncForEach(event.Records, async record => {
+    const message = parseMessage(record);
+    log("SQS Message: ", message);
 
-        const invalidationParams = {
-            DistributionId: message.distribution_id,
-            InvalidationBatch: {
-                CallerReference: new Date().getTime().toString(),
-                Paths: {
-                    Quantity: 1,
-                    Items: [message.path]
-                }
-            }
-        };
+    if (!message.distribution_id || !message.path) {
+      logWarning(
+        'bad format. desired SNS message format: {"distribution_id": "<distid>", "path": "/a/path/*"}'
+      );
+      return Promise.resolve();
+    }
 
-        cloudfront.createInvalidation(invalidationParams, (err, data) => {
-            if (err) {
-                if (err.code !== "TooManyInvalidationsInProgress") {
-                    const msg = `[WARNING] ignoring error: ${err}.`;
-                    console.log(msg);
-                    callback(null, msg);
-                    return;
-                }
-
-                let retried = message.retry_count || 0;
-                if (retried > NUM_OF_RETRIES - 1) {
-                    const msg = `[WARNING] Failed after ${NUM_OF_RETRIES} retries`;
-                    console.log(msg);
-                    callback(null, msg);
-                    return;
-
-                } else {
-                    retried++;
-                    message.retry_count = retried;
-
-                    const arn = record.eventSourceARN.split(":", 6);
-                    const queueUrl =
-                        "https://sqs." + arn[3] + ".amazonaws.com/" + arn[4] + "/" + arn[5];
-
-                    // This Message must be in SNS => SQS Format for incoming parsing to work
-                    const params = {
-                        MessageBody: JSON.stringify({Message: JSON.stringify(message)}),
-                        QueueUrl: queueUrl,
-                        DelaySeconds: RETRY_TIMOUT
-                    };
-                    const msg = `[RETRY] retrying ${invalidationParams.DistributionId}:${
-                        invalidationParams.Paths
-                        }`;
-                    console.log(params);
-
-                    sqs.sendMessage(params, (err, data) => {
-                        if (err) {
-                            console.log(err);
-                            callback("failed to send message for retry");
-                            return;
-                        }
-                        const msg = `[RETRY] Scheduling a retry in ${RETRY_TIMOUT} seconds. ${retried} retries`;
-                        console.log(msg);
-                        callback(null, msg);
-                        return;
-
-                    });
-                }
-            } else
-                console.log(data);
-
-        });
-
-    });
-
-    callback(null, "A-OK");
+    try {
+      const successfulInvalidation = await createCloudfrontInvalidation(
+        makeInvalidationParams(message)
+      );
+      log(successfulInvalidation);
+      return Promise.resolve();
+    } catch (e) {
+      return await handleInvalidationError(e);
+    }
+  });
+  return "A-OK";
 };
